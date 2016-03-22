73f132d8c506a0f982f16d0fa1a659ea54b313a2
ibadboy <polawat.job@gmail.com>
2016-03-22 06:11:32 +0000

    database config

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'hosting';
+$active_group = 'DEV';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -95,12 +95,34 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-$db['hosting'] = array(
+$db['PROD'] = array(
 	'dsn'	=> '',
 	'hostname' => 'mysql.hostinger.in.th',
 	'username' => 'u783825707_tour',
 	'password' => 'tour1234',
 	'database' => 'u783825707_tour',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['DEV'] = array(
+	'dsn'	=> '',
+	'hostname' => '110.170.171.138',
+	'username' => 'nida',
+	'password' => '1234567890',
+	'database' => 'tour_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
