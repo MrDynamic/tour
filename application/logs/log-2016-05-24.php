ff7858816d424eb268ba80964b068e7717fec0e9
ibadboy <polawat.th@gmail.com>
2016-05-24 21:50:55 +0700

    move document

---
+++ application/logs/log-2016-05-24.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2016-05-24 21:45:01 --> UTF-8 Support Enabled
+DEBUG - 2016-05-24 21:45:01 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-24 21:45:01 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-24 21:45:01 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-24 21:45:01 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-24 21:45:02 --> UTF-8 Support Enabled
+DEBUG - 2016-05-24 21:45:02 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-24 21:45:02 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-24 21:45:02 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-24 21:45:02 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-24 21:45:02 --> [authen > login] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-24 21:45:02 --> [authen > login] Start  >> 
+DEBUG - 2016-05-24 21:45:02 --> Total execution time: 0.0272
