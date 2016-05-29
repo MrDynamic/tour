e25b0a3566bdd0e064b63f64909516cebac9c441
ibadboy <polawat.th@gmail.com>
2016-05-29 09:52:01 +0700

    change

---
+++ application/logs/log-2016-05-29.php
@@ -0,0 +1,244 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2016-05-29 09:47:10 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:47:10 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:47:10 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:47:10 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:10 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:10 --> [package > index] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:47:10 --> Pagination class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:10 --> Total execution time: 0.0795
+DEBUG - 2016-05-29 09:47:15 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:47:15 --> No URI present. Default controller set.
+DEBUG - 2016-05-29 09:47:15 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:47:15 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:47:15 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:15 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:15 --> [home > index] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:47:15 --> Total execution time: 0.0277
+DEBUG - 2016-05-29 09:47:21 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:47:21 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:47:21 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:47:21 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:21 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:21 --> [user > register] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:47:21 --> Total execution time: 0.0353
+DEBUG - 2016-05-29 09:47:50 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:47:50 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:47:50 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:47:50 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:50 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:50 --> [user > generateAmphur] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:47:50 --> [user > generateAmphur] post data  >> Array
+(
+    [provinceId] => 1
+)
+
+DEBUG - 2016-05-29 09:47:50 --> [user > generateAmphur] Province ID  >> 1
+DEBUG - 2016-05-29 09:47:50 --> [user > generateAmphur] amphur data  >> Array
+(
+    [] => กรุณาเลือก
+    [1] => เขตพระนคร   
+    [2] => เขตดุสิต   
+    [3] => เขตหนองจอก   
+    [4] => เขตบางรัก   
+    [5] => เขตบางเขน   
+    [6] => เขตบางกะปิ   
+    [7] => เขตปทุมวัน   
+    [8] => เขตป้อมปราบศัตรูพ่าย   
+    [9] => เขตพระโขนง   
+    [10] => เขตมีนบุรี   
+    [11] => เขตลาดกระบัง   
+    [12] => เขตยานนาวา   
+    [13] => เขตสัมพันธวงศ์   
+    [14] => เขตพญาไท   
+    [15] => เขตธนบุรี   
+    [16] => เขตบางกอกใหญ่   
+    [17] => เขตห้วยขวาง   
+    [18] => เขตคลองสาน   
+    [19] => เขตตลิ่งชัน   
+    [20] => เขตบางกอกน้อย   
+    [21] => เขตบางขุนเทียน   
+    [22] => เขตภาษีเจริญ   
+    [23] => เขตหนองแขม   
+    [24] => เขตราษฎร์บูรณะ   
+    [25] => เขตบางพลัด   
+    [26] => เขตดินแดง   
+    [27] => เขตบึงกุ่ม   
+    [28] => เขตสาทร   
+    [29] => เขตบางซื่อ   
+    [30] => เขตจตุจักร   
+    [31] => เขตบางคอแหลม   
+    [32] => เขตประเวศ   
+    [33] => เขตคลองเตย   
+    [34] => เขตสวนหลวง   
+    [35] => เขตจอมทอง   
+    [36] => เขตดอนเมือง   
+    [37] => เขตราชเทวี   
+    [38] => เขตลาดพร้าว   
+    [39] => เขตวัฒนา   
+    [40] => เขตบางแค   
+    [41] => เขตหลักสี่   
+    [42] => เขตสายไหม   
+    [43] => เขตคันนายาว   
+    [44] => เขตสะพานสูง   
+    [45] => เขตวังทองหลาง   
+    [46] => เขตคลองสามวา   
+    [47] => เขตบางนา   
+    [48] => เขตทวีวัฒนา   
+    [49] => เขตทุ่งครุ   
+    [50] => เขตบางบอน   
+)
+
+DEBUG - 2016-05-29 09:47:50 --> [user > generateAmphur] response  >> <option value=''>กรุณาเลือก</option><option value='1'>เขตพระนคร</option><option value='2'>เขตดุสิต</option><option value='3'>เขตหนองจอก</option><option value='4'>เขตบางรัก</option><option value='5'>เขตบางเขน</option><option value='6'>เขตบางกะปิ</option><option value='7'>เขตปทุมวัน</option><option value='8'>เขตป้อมปราบศัตรูพ่าย</option><option value='9'>เขตพระโขนง</option><option value='10'>เขตมีนบุรี</option><option value='11'>เขตลาดกระบัง</option><option value='12'>เขตยานนาวา</option><option value='13'>เขตสัมพันธวงศ์</option><option value='14'>เขตพญาไท</option><option value='15'>เขตธนบุรี</option><option value='16'>เขตบางกอกใหญ่</option><option value='17'>เขตห้วยขวาง</option><option value='18'>เขตคลองสาน</option><option value='19'>เขตตลิ่งชัน</option><option value='20'>เขตบางกอกน้อย</option><option value='21'>เขตบางขุนเทียน</option><option value='22'>เขตภาษีเจริญ</option><option value='23'>เขตหนองแขม</option><option value='24'>เขตราษฎร์บูรณะ</option><option value='25'>เขตบางพลัด</option><option value='26'>เขตดินแดง</option><option value='27'>เขตบึงกุ่ม</option><option value='28'>เขตสาทร</option><option value='29'>เขตบางซื่อ</option><option value='30'>เขตจตุจักร</option><option value='31'>เขตบางคอแหลม</option><option value='32'>เขตประเวศ</option><option value='33'>เขตคลองเตย</option><option value='34'>เขตสวนหลวง</option><option value='35'>เขตจอมทอง</option><option value='36'>เขตดอนเมือง</option><option value='37'>เขตราชเทวี</option><option value='38'>เขตลาดพร้าว</option><option value='39'>เขตวัฒนา</option><option value='40'>เขตบางแค</option><option value='41'>เขตหลักสี่</option><option value='42'>เขตสายไหม</option><option value='43'>เขตคันนายาว</option><option value='44'>เขตสะพานสูง</option><option value='45'>เขตวังทองหลาง</option><option value='46'>เขตคลองสามวา</option><option value='47'>เขตบางนา</option><option value='48'>เขตทวีวัฒนา</option><option value='49'>เขตทุ่งครุ</option><option value='50'>เขตบางบอน</option>
+DEBUG - 2016-05-29 09:47:50 --> Total execution time: 0.0341
+DEBUG - 2016-05-29 09:47:54 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:47:54 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:47:54 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:47:54 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:54 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:47:54 --> [user > generateAmphur] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:47:54 --> [user > generateAmphur] post data  >> Array
+(
+    [provinceId] => 
+)
+
+DEBUG - 2016-05-29 09:47:54 --> [user > generateAmphur] Province ID  >> 
+DEBUG - 2016-05-29 09:47:54 --> [user > generateAmphur] amphur data  >> Array
+(
+    [] => กรุณาเลือก
+)
+
+DEBUG - 2016-05-29 09:47:54 --> [user > generateAmphur] response  >> <option value=''>กรุณาเลือก</option>
+DEBUG - 2016-05-29 09:47:54 --> Total execution time: 0.0263
+DEBUG - 2016-05-29 09:48:00 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:48:00 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:48:00 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:48:00 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:48:00 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:48:00 --> [authen > submitLogin] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:48:00 --> [authen > submitLogin] login data  >> Array
+(
+    [username] => admin
+    [password] => admin
+)
+
+DEBUG - 2016-05-29 09:48:00 --> [authen > submitLogin] session data  >> Array
+(
+    [user_id] => 1
+    [username] => admin
+    [role] => ADMIN
+)
+
+DEBUG - 2016-05-29 09:48:00 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:48:00 --> No URI present. Default controller set.
+DEBUG - 2016-05-29 09:48:00 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:48:00 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:48:00 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:48:00 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:48:00 --> [home > index] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:48:00 --> Total execution time: 0.0254
+DEBUG - 2016-05-29 09:50:12 --> UTF-8 Support Enabled
+DEBUG - 2016-05-29 09:50:12 --> No URI present. Default controller set.
+DEBUG - 2016-05-29 09:50:12 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2016-05-29 09:50:12 --> Config file loaded: /Users/MacBookPro/Documents/i-work/root/tour/application/config/config_authen.php
+DEBUG - 2016-05-29 09:50:12 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:50:12 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2016-05-29 09:50:12 --> [home > index] config  >> Array
+(
+    [0] => checkoutPage
+    [1] => checkout
+    [2] => submitToPaypal
+    [3] => userPage
+    [4] => orderListPage
+    [5] => requestTour
+    [6] => requestTourPage
+    [7] => editRequestTour
+    [8] => listRequestTour
+)
+
+DEBUG - 2016-05-29 09:50:12 --> Total execution time: 0.0335
