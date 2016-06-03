0baebb2a58ce3df1695cd202cd8ba22f09ab91b8
ibadboy <polawat.job@gmail.com>
2016-06-03 05:39:24 +0000

    notify order

--- application/config/config.php
+++ application/config/config.php
@@ -368,9 +368,9 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'native';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 0;
+$config['sess_expiration'] = 900;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
